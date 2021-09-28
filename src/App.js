a5c3f0332fd4eb364f451b49b6bf1df86cefc6e0
ggghhh27 <91445798+ggghhh27@users.noreply.github.com>
2021-09-28 16:56:23 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './tailwind.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="block w-32 h-32">
+      <p className="text-right text-9xl">Text test</p>
+      <span className="w-32 h-32 bg-red-500 text-green-600 rounded-lg">Text test2</span>
+      <ul className="block w-72 h-32 bg-gradient-to-r from-yellow-200 via-indigo-500 to-pink-300 text-center">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: '',
+  },
+  output: {
+    path: '',
+    filename: '',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+        {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+        },
+        {
+        test: /.css?$/,
+        exclude: [],
+        //로더는 오른쪽부터 읽어들이므로 postcss-loader를 맨 오른쪽에 넣어준다.
+        use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        ],
+  },
+  plugins: [],
+  optimization: {},
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+};
